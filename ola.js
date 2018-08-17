0bc9765bc888c7e4a4ac9a516cae236cc7e45fce
Arthur Alves <32318596+arthur-alvess@users.noreply.github.com>
2018-08-17 08:32:33 -0300

    Create ola.js

---
+++ ola.js
@@ -0,0 +1,5 @@
+var nome = 'Arthur';
+var data = new Date();
+var dias = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+var ds = data.getDay();
+console.log("Ol\u00E1, " + nome + ",\n hoje \u00E9 " + dias[ds] + "!");
